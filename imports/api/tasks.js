29c67c28c5304a54f093e68d3dfecc6c4535f237
yramocan <yramocan@umich.edu>
2016-05-01 13:32:51 -0400

    Separate database code from client. Secure CRUD activity through user authentication

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,19 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
@@ -24,6 +37,12 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
     Tasks.remove(taskId);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
   },
 
   'tasks.setChecked'(taskId, setChecked) {
@@ -31,5 +50,22 @@ Meteor.methods({
     check(setChecked, Boolean);
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
+
 
 // Task component - represents a single todo item.
 export default class Task extends Component {
@@ -12,20 +14,42 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+  }
+
   render() {
     // Give tasks a different className when they are checked off
     // to add styles in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
+
+    console.log(this.props.currentUser);
 
     return (
       <li className={ taskClassName }>
-        <span className="delete" onClick={ this.deleteThisTask.bind(this) }>&times; </span>
+        <span>
+          <span className="check-complete">
+            <input type="checkbox" readOnly checked={ this.props.task.checked } onClick={ this.toggleChecked.bind(this) } />
+          </span>
 
-        <input type="checkbox" readOnly checked={ this.props.task.checked } onClick={ this.toggleChecked.bind(this) }></input>
-
-        <span className="text">
-          <strong> { this.props.task.username }</strong>: { this.props.task.text }
+          <span className="delete" onClick={ this.deleteThisTask.bind(this) }>&times;</span>
         </span>
+        
+        <span className="text">
+          <strong> { this.props.task.username }</strong>
+          : { this.props.task.text }
+        </span>
+
+        { this.props.showPrivateButton ? (
+            <span>
+              <button className="toggle-private" onClick={ this.togglePrivate.bind(this) }>
+                { this.props.task.private ? 'Private' : 'Public' }
+              </button>
+            </span>
+        ) : '' }
       </li>
     );
   }
@@ -35,4 +59,5 @@ Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required.
   task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
 };
